d5df1a4d04b307f6de74f9366560aadae74871fe
Ersan <ersanaksahin@gmail.com>
2025-02-24 07:30:19 +0300

    Add files via upload

---
+++ deneme.py
@@ -0,0 +1,11 @@
+print("Merhaba, Python!")
+
+ad = input("Adınız nedir ? ")
+yas = input("kaç yaşındasınız ?")
+boy = float(input("boyunuz kaç metre ?"))
+
+# Bİlgileri ekrana yazdır
+print("\nKullanıcı Bilgileri")
+print("Ad:", ad)
+print("Yaş:", yas)
+print("Boy:", boy)
