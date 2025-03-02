c0512f10bf085f184b9bdb062f7ae0a68b7df90f
Ersan <ersanaksahin@gmail.com>
2025-03-02 22:22:55 +0300

    Add files via upload

---
+++ def.py
@@ -0,0 +1,14 @@
+def selamlama(isim):
+    print(f"merhaba, {isim}!")
+
+
+selamlama("olcay")
+selamlama("KurtKobein")
+
+
+def kare_al(sayi):
+    return sayi**2
+
+
+sonuc = kare_al(5)
+print(sonuc)  # 25
