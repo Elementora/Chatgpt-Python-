31a63dc876c89081ccf90d3e6dc3d401e504e862
Ersan <ersanaksahin@gmail.com>
2025-03-07 23:37:27 +0300

    Update Palindrom.py

--- Palindrom.py
+++ Palindrom.py
@@ -1,5 +1,8 @@
 
 # Kullanıcının Girdiği Sayıların Ortalamasını Hesaplayan Program
+import random
+
+
 def ortalama_hesaplama(sayilar):
     if len(sayilar) == 0:
         return sum(sayilar) / len(sayilar)
@@ -29,3 +32,42 @@ if palindrom_mu(kelime):
     print(f"{kelime} bir palindromdur!")
 else:
     print(f"{kelime} bir polindrom değildir.")
+
+
+###########################################################################
+
+#  Rastgele Sayı Üreten ve Kullanıcıdan Tahmin Alan Oyun
+
+
+def sayi_tahmin_oyunu():
+    rastgele_sayi = random.random(1, 100)
+    tahmin = -1
+
+    while tahmin != rastgele_sayi:
+        tahmin = int(input("Tahmini bir sayı girin (1-100): "))
+
+        if tahmin < rastgele_sayi:
+            print("Daha büyük bir sayı girin !")
+        elif tahmin > rastgele_sayi:
+            print("Daha küük bir sayı girin !")
+    print("Tebrikler ! Doğru tahmin ettiniz .")
+
+    sayi_tahmin_oyunu()
+
+############################################################################
+
+# Kullanıcının girdiği bir metindeki sesli harfleri bulan bir program
+
+
+sesliharfler = ["a", "e", "ı", "i", "o", "ö", "u", "ü"]
+
+cumle = input(" Bir cümle girin : ")
+
+sesli_harf_sayisi = 0
+
+for harf in cumle:
+    if harf.lower() in sesliharfler:
+        print(harf, end=" ,")
+        sesli_harf_sayisi += 1
+
+print(f"\nToplam  {sesli_harf_sayisi}, sesli harf var")
