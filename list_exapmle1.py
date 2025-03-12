e8654c62889cffe3a03f380c88ac8b1dfa19c3b7
Ersan <ersanaksahin@gmail.com>
2025-03-12 23:51:33 +0300

    Update list_exapmle1.py

--- list_exapmle1.py
+++ list_exapmle1.py
@@ -8,4 +8,21 @@ def sum_list(numbers):
     my_list = [10, 20, 30, 40, 50]
     print("Toplam", sum_list(my_list))  # Çıktı: 150
 
+# Bu fonksiyon bir liste alıyor, döngü ile tüm elemanlarını topluyor ve sonucu döndürüyor.
+
     #####################################################
+
+# Liste Filtreleme Fonksiyonu
+# Çift Sayıları Filtreleme
+
+
+def filter_even_numbers(numbers):
+    even_numbers = []
+    for num in numbers:
+        if num % 2 == 0:
+            even_numbers.appent(num)
+            return even_numbers
+
+        my_list = [3, 6, 9, 12, 15, 18]
+        print("Çift sayılar", filter_even_numbers(my_list))
+        # Çıktı: [6, 12, 18]
