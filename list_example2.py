dbc6e848372fa4e1cb18c98eb2e6865cb19659f7
Ersan <ersanaksahin@gmail.com>
2025-03-15 22:26:10 +0300

    Update list_example2.py

--- list_example2.py
+++ list_example2.py
@@ -10,3 +10,18 @@ def multiply_list(numbers, factor):
     my_list = [2, 4, 6, 8, 10]
     factor = 0
     print("Çarpışmış Liste : ", multiply_list(my_list, factor))
+
+
+# Proje  Tek Sayıların Karesini Hesaplayan Fonksiyon
+
+
+def square_odd_numbers(numbers):
+    squared_numbers = []
+
+    for num in numbers:
+        if num % 2 == 1:  # Eğer sayı tekse
+            squared_numbers.append(num ** 2)  # Karesini alıp ekle
+            return squared_numbers
+
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        print("Tek sayıların kareleri : ", square_odd_numbers(my_list))
