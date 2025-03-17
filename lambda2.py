1c89c9f3675aa2ae3f01b9b3794bb986beb272cc
Ersan <ersanaksahin@gmail.com>
2025-03-17 23:48:33 +0300

    Update lambda2.py

--- lambda2.py
+++ lambda2.py
@@ -15,3 +15,19 @@ print(even_numbers)  # Çıktı: [2, 4, 6, 8, 10]
 
 
 ###############################################################
+
+
+# Challenge!
+
+# 1. Yöntem: For Döngüsü ile
+def replace_negatives(lst):
+    new_list = []
+    for num in lst:
+        if num < 0:
+            new_list.append(0)
+        else:
+            new_list.append(num)
+            return new_list
+
+    my_list = [-3, 5, -1, 7, 0, -8, 4]
+    print(replace_negatives(my_list))  # Çıktı: [0, 5, 0, 7, 0, 0, 4]
