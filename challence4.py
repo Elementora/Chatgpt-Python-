37df320dc540359eabd5682fc022277d50197489
Ersan <ersanaksahin@gmail.com>
2025-03-18 22:30:12 +0300

    Add files via upload

---
+++ challence4.py
@@ -0,0 +1,12 @@
+##########################################################
+# Challenge: 3’ün Katlarını "Fizz", 5’in Katlarını "Buzz" Yazan Program
+
+def fizzbuzz(lst):
+    return ["FizzBuzz" if num % 3 == 0 and num % 5 == 0 else
+            "Fizz" if num % 3 == 0 else
+            "Buzz" if num % 5 == 0 else
+            num for num in lst]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 18, 20, 30]
+print(fizzbuzz(numbers))
